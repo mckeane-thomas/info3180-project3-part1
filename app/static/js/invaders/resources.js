ab05630f849ff64aad8c10b19ded5aa3ecac5ba1
mckeane-thomas <mckeane.thomas@gmail.com>
2015-03-23 01:25:47 +0000

    commiting to repo

---
+++ app/static/js/invaders/resources.js
@@ -0,0 +1,12 @@
+game.resources = [
+    {
+        "name": "player",
+        "type": "image",
+        "src": "/static/invaders_data/img/player.png"
+    },
+    {
+        "name": "ships",
+        "type": "image",
+        "src": "/static/invaders_data/img/ships.png"
+    }
+];
